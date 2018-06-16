08cfa4cd70a0b6128722a2073e7ca255b0a26ea6
Dan Sytnikov <dan.sytnikov@gmail.com>
2018-06-16 10:08:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/lkdjf/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.user.lkdjf;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.bitstamp) {
+            Intent intentfr = new Intent(this, Activity_Bitstamp.class);
+            startActivity(intentfr);
+        }
+        if (view.getId() == R.id.bitfinex) {
+            Intent intentB = new Intent(this, Activity_Bitfinex.class);
+            startActivity(intentB);
+        }
+    }
+}
+
+
+
+
